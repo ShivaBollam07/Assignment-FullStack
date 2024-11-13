a3047bb7b1784e10ce0e6276fac1084f920aabdb
Bollam Shiva Shankara Varaprasad <shivabollam07@gmail.com>
2024-11-14 02:38:39 +0530

    first commit

---
+++ students/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    marks = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.name
---
+++ students/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
+
+# View to add a new student
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'students/add_student.html', {'form': form})
+
+# View to list all students with edit/delete options
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'students/student_list.html', {'students': students})
+
+# View to edit a student's details
+def edit_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'students/edit_student.html', {'form': form})
+
+# View to delete a student
+def delete_student(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+    return render(request, 'students/delete_student.html', {'student': student})
